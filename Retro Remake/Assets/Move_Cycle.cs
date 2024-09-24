47e980e9ce3182ac5111058d56b4ccb69d6de482
Ward <1720967@mukilteo.wednet.edu>
2024-09-24 13:31:56 -0700

    redid moving objs, fixed cycle code, removed homes and some logs/lilypads

--- Retro Remake/Assets/Move_Cycle.cs
+++ Retro Remake/Assets/Move_Cycle.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 
 public class Move_Cycle : MonoBehaviour
 {
-    public Vector2 direction = Vector2.right;
+    public Vector3 direction = Vector3.right; 
     public float speed = 1f;
     public int size = 1;
 
-    private Vector2 leftEdge;
-    private Vector2 rightEdge;
+    private Vector3 leftEdge;
+    private Vector3 rightEdge;
     public void Start()
     {
-        leftEdge = Camera.main.ViewportToWorldPoint(Vector2.zero);
-        rightEdge = Camera.main.ViewportToWorldPoint(Vector2.right);
+        leftEdge = Camera.main.ViewportToWorldPoint(Vector3.left);
+        rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
     }
     private void Update()
     {
         if (direction.x > 0 && (transform.position.x - size) > rightEdge.x)
         {
-            Vector2 position = transform.position;
+            Vector3 position = transform.position;
             position.x = leftEdge.x - size;
             transform.position = position;
         }
-        if (direction.x < 0 && (transform.position.x +size) > leftEdge.x)
+        else if (direction.x < 0 && (transform.position.x + size) < leftEdge.x)
         {
-            Vector2 position = transform.position;
+            Vector3 position = transform.position;
             position.x = rightEdge.x + size;
             transform.position = position;
         }
-       transform.Translate(direction * speed * Time.deltaTime);
-        
+        else
+        {
+            transform.Translate(speed * Time.deltaTime * direction);
+        }
     }
 
   }
