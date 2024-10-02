05493271ba6a13c0d90acce411166d02e89bfbe7
Ward <1720967@mukilteo.wednet.edu>
2024-10-02 13:20:33 -0700

    fixed the scene loading sequence

--- Retro Remake/Assets/Move_Cycle.cs
+++ Retro Remake/Assets/Move_Cycle.cs
@@ -1,5 +1,5 @@
 using System.Net.Security;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 
 public class Move_Cycle : MonoBehaviour
--- Retro Remake/Assets/SceneLoader.cs
+++ Retro Remake/Assets/SceneLoader.cs
@@ -1,27 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        // SceneManager.LoadScene("Title Screen");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+  
     public void LoadLevel()
     {
         SceneManager.LoadScene("duckplswork");
     }
+    
 } 
 
 
